fc5b14747b23637e36aa8bd1c22c507bde838b4b
Prioli <priolilucas1@gmail.com>
2020-10-13 17:15:27 -0300

    Connected database and migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,38 @@
 import express from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from './models/Orphanage'
+
+import './database/connection'
 
 const app= express();
 app.use(express.json())
 
-app.get('/users', (request, response)=>{
-  return response.json({message: 'Hello World'})
+app.post('/orphanages', async (request, response)=>{
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({ //Orfanato pré-criado
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  })
+
+  await orphanagesRepository.save(orphanage) //Salvar no BD
+
+  return response.json(orphanage)
 })
 
 app.listen(3333)
